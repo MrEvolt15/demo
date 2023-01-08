b8678d45c16de48164741e7f355f8cd65b6ad90b
Carmen617 <122191170+Carmen617@users.noreply.github.com>
2023-01-08 00:25:47 -0500

    Add files via upload

---
+++ AnulaFactura.java
@@ -0,0 +1,27 @@
+package com.example.facturacion;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.event.*;
+
+public class AnulaFactura extends JFrame{
+    AnulaFactura(){
+        JButton back = new JButton("Regresar");
+        back.setBounds(10,80,100,25);
+        back.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                new Menu();
+                dispose();
+            }
+        });
+        add(back);
+
+        setLocation(300, 200);
+        setSize(400,500);//400 width and 500 height  
+        setLayout(null);//using no layout managers  
+        setVisible(true);//making the frame visible 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+System.out.println("hola te llamas yo no c");
+    }
+}
